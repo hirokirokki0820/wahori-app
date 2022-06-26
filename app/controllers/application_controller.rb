e210d3c97d61a7bc73fc0bc68e8c2390a9861a96
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-26 19:54:51 +0900

    Add: メール認証に必要な設定を追加(Userモデル作成,SMTPサーバー設定など)
    Gmail サーバーを使った認証：成功

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:index]
+
+  #新規登録時のストロングパラメータに「nameカラム」の追加
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -23,9 +23,9 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
-  # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+  # 送信者メールアドレスの設定
+  config.mailer_sender = 'info@kobacchi.com'
+  # config.mailer_sender = 'kobacchi0820@gmail.com'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
