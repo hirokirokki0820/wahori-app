7de7f6586148305a84ab3da134375930884f70d8
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-27 00:07:44 +0900

    Fix: 7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => 587,
-    :domain => 'heroku.com',
+    :domain => 'wahori-app.heroku.com',
     :user_name => 'apikey',
     :password => ENV['SENDGRID_APIKEY'],
     :authentication => :plain,
